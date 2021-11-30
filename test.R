04a1776dfeb266c331b0f1d564a9fe5b42d6c75d
tommygorham <tommy.gorham97@gmail.com>
2021-11-30 15:28:58 -0500

    cleanup / make more usable

--- test.R
+++ test.R
@@ -14,13 +14,11 @@ mycountries = c("United.States" ) #add more countries later to compare cumulativ
 TB1 <- TB_jhucases %>%
   select(c('date', mycountries))
 
-
 #select a timeframe
-#TB1 %>% filter(date > ymd("2020-1-22"), date < ymd("2020-2-22"))
-#select a timeframe method 2 
-start = ymd("2021-11-22")
+start = Sys.Date()-7
+ymd(start)
 end = start + ddays(7)
-#TB1 %>% filter(date > start, date < end)
+
 #plot this timeframe 
 TB_sub <- TB1 %>% dplyr::filter( date >= start, date < end) %>% dplyr::select( c(1:2), date)
 head(TB_sub)
@@ -28,17 +26,9 @@ tail(TB_sub)
 
 ggplot(TB_sub, aes( x=date, y=TB_sub[,2])) + geom_point()
 
-#ggplot(TB1, aes(x=date, y=TB1[,2]))  + geom_point() 
+#TO: Daily Cases, overlay vaccine
 
 
 
 
-
-
-TB_jhucases[,1] #gets all dates (aka first col)
-
-TB_jhucases[(TB_jhucases[,1])]
-
-
-names(TB_jhucases[1,1])
 
