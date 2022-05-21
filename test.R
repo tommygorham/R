71c5edcaa82b3625909e2e82e67e794679468d59
Tommy Gorham <38857089+tommygorham@users.noreply.github.com>
2022-05-21 17:41:43 -0400

    Update test.R

--- test.R
+++ test.R
@@ -23,12 +23,4 @@ end = start + ddays(7)
 TB_sub <- TB1 %>% dplyr::filter( date >= start, date < end) %>% dplyr::select( c(1:2), date)
 head(TB_sub)
 tail(TB_sub)
-
 ggplot(TB_sub, aes( x=date, y=TB_sub[,2])) + geom_point()
-
-#TO: Daily Cases, overlay vaccine
-
-
-
-
-
